8df415465368ed9cde26ef8c0853c1a66691cd97
sleepyliii <156730259+sleepyliii@users.noreply.github.com>
2024-06-28 00:18:47 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,11 @@
-import React, { useState, useEffect, useRef } from "react";
+// src/App.js
+
+import React, { useState, useRef } from "react";
 import axiosInstance from "./axiosInstance";
-import { Button, Divider, Input, Space, Typography } from "antd";
+import { Button, Input, Typography } from "antd";
 import { DownloadOutlined } from "@ant-design/icons";
+import Sidebar from "./components/Sidebar";
+import "./App.css";
 
 function App() {
   const { TextArea } = Input;
@@ -61,61 +65,9 @@ function App() {
   };
 
   return (
-    <div className="flex h-screen bg-gray-200">
-      <div className="w-1/4 p-4 border-r border-gray-300 bg-gray-100">
-        <Typography.Title level={1}>Sphinx RST Generator</Typography.Title>
-        <Typography.Title level={5}>Add Title</Typography.Title>
-        <Space>
-          <Input
-            placeholder="please enter the title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-          <Button type="primary" onClick={addTitle}>
-            Submit
-          </Button>
-        </Space>
-        <Divider />
-
-        <Typography.Title level={5}>Add Content</Typography.Title>
-        <Space>
-          <TextArea
-            rows={5}
-            placeholder="please enter the content, max length 400"
-            maxLength={400}
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
-          <Button type="primary" onClick={addContent}>
-            Submit
-          </Button>
-        </Space>
-        <Divider />
-
-        <Typography.Title level={5}>Add Reference</Typography.Title>
-        <Space>
-          <Input
-            placeholder="reference name"
-            value={reference.name}
-            onChange={(e) =>
-              setReference({ ...reference, name: e.target.value })
-            }
-          />
-          <Input
-            placeholder="reference link"
-            value={reference.link}
-            onChange={(e) =>
-              setReference({ ...reference, link: e.target.value })
-            }
-          />
-          <Button type="primary" onClick={addReference}>
-            Submit
-          </Button>
-        </Space>
-        <Divider />
-      </div>
-
-      <div className="flex flex-col w-3/4 p-4 bg-gray-50">
+    <div className="flex h-screen">
+      <Sidebar />
+      <div className="flex flex-col w-full p-4 bg-gray-50">
         <div className="flex justify-between items-start">
           <div className="flex-1">
             <Typography.Title level={5}>Generated RST</Typography.Title>
@@ -144,4 +96,3 @@ function App() {
 }
 
 export default App;
-
